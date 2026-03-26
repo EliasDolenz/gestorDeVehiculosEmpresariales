f49b0cb679b6ca06b7b3a92599e49551e57bc9f6
Elias <elissdolenz@gmail.com>
2026-03-25 21:00:18 -0300

    Primer commit

---
+++ src/main/java/gestorDeVehiculosEmpresariales/GestorDeVehiculosEmpresarialesApplication.java
@@ -0,0 +1,13 @@
+package gestorDeVehiculosEmpresariales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeVehiculosEmpresarialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeVehiculosEmpresarialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestorDeVehiculosEmpresariales/GestorDeVehiculosEmpresarialesApplicationTests.java
@@ -0,0 +1,13 @@
+package gestorDeVehiculosEmpresariales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeVehiculosEmpresarialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
